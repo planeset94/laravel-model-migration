1dea3b29ee81de5ef532a29770a7e5aadf90771b
planeset94 <planeset94@gmail.com>
2021-07-13 16:03:40 +0200

    create DB (holidays) and one table

---
+++ laravel/database/migrations/2021_07_13_135910_create_trip_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trip', function (Blueprint $table) {
+            $table->id();
+            $table->destination();
+            $table->duration();
+            $table->transport_required();
+            $table->people_number();
+            $table->price();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trip');
+    }
+}
